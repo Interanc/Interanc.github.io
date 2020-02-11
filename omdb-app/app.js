94e640ca91bf9a61ef9c6aeb16cabc02aa31e112
Jamal McDowell <jamal.mcdowell@gmail.com>
2020-02-10 19:09:40 -0500

    added info pane on click, and remove pane on click. Need to enlarge image when it is clicked on

--- omdb-app/app.js
+++ omdb-app/app.js
@@ -15,14 +15,31 @@ $(() => {
 
             const amiiboList = () => {
                     amiiboArray.push(data.amiibo)
-                        // console.log(amiiboArray[0][0].image);
-                        for (var j = 0; j < 30; j++) {
+                        // console.log(amiiboArray[0][0]);
+                        for (var j = 0; j < amiiboArray[0].length; j++) {
                             const $amiiboBox = $("<div>").addClass("amiiboBox")
                             let $amiiboImage = $("<img>").attr("src", amiiboArray[0][j].image).appendTo($amiiboBox)
+                            let $amiiboName = $("<p>").text(amiiboArray[0][j].character).appendTo($amiiboBox)
+                            let $amiiboCounter = $("<p>").text(j)
                             $($amiiboBox).on("click", () => {
-
                                 $amiiboBox.prependTo("body")
+                                j = $($amiiboCounter).text();
+                                let $amiiboTable = $("<table>").appendTo($amiiboBox).addClass("infoTable")
+                                let $amiiboSeriesHolder = $("<tr>").appendTo($amiiboTable)
+                                let $amiiboSeriesTitle = $("<th>").text("Amiibo Series ").appendTo($amiiboSeriesHolder)
+                                let $amiiboSeries = $("<th>").text(amiiboArray[0][j].amiiboSeries).appendTo($amiiboSeriesHolder)
+                                let $amiiboGameSeriesHolder = $("<tr>").appendTo($amiiboTable)
+                                let $amiiboGameSeriesTitle = $("<th>").text("Game Series  ").appendTo($amiiboGameSeriesHolder)
+                                let $amiiboGameSeries = $("<th>").text(amiiboArray[0][j].gameSeries).appendTo($amiiboGameSeriesHolder)
+                                let $amiiboReleaseDateHolder = $("<tr>").appendTo($amiiboTable)
+                                let $amiiboReleaseDateTitle = $("<th>").text("Release Date ").appendTo($amiiboReleaseDateHolder)
+                                let $amiiboReleaseDate = $("<th>").text(amiiboArray[0][j].release.na).appendTo($amiiboReleaseDateHolder)
+                                let $amiiboTableImage = $("<tr>").appendTo($amiiboTable)
+                                let $amiiboSeriesImage = $("<th>").text("Image ").appendTo($amiiboTableImage)
+                                let $amiiboImagePlacer = $("<th>").appendTo($amiiboTableImage)
+                                let $amiiboBigImage = $("<img>").attr("src", amiiboArray[0][j].image).appendTo($amiiboImagePlacer)
                                 $($amiiboBox).on("click", () => {
+                                    $(".infoTable").empty()
                                     $amiiboBox.prependTo($amiiboWrapper)
                                 })
                             })
